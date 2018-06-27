cdb9259713ebc29d8a1c28b7c7fe1b54b6d760fb
Alex Clement <MrClement@users.noreply.github.com>
2018-06-27 14:11:32 -0600

    First pass on install script add some useful tweaks for OSX

---
+++ setup.sh
@@ -0,0 +1,8 @@
+apt get install -y zsh
+sh resources/oh_my_zsh_install.sh
+mkdir -p ~/.vim/autoload ~/.vim/bundle && cp resources/pathogen.vim ~/.vim/autoload/pathogen.vim
+cp -r resources/vim-colors-solarized ~/.vim/bundle
+cp .vimrc ~/.vimrc
+cp .zshrc ~/.zshrc
+cp .gitconfig ~/.gitconfig
+
